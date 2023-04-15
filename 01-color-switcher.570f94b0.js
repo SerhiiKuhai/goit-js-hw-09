d7ba16a169fe8718d103c0c4d9a01e14c9bbe4e7
SerhiiKuhai <117837995+SerhiiKuhai@users.noreply.github.com>
2023-04-15 11:58:44 +0000

    Deploying to gh-pages from @ SerhiiKuhai/goit-js-hw-09@db61134936dd9f44dfd0e86a03bae97b40f5521c 🚀

---
+++ 01-color-switcher.570f94b0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let r=null;function a(){e.setAttribute("disabled","disabled")}a(),t.addEventListener("click",(()=>{r=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),a()}));
+//# sourceMappingURL=01-color-switcher.570f94b0.js.map
