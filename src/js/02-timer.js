eddc96b2b606cb97bb7de2c8fefd9e20ef4df0cb
SerhiiKuhai <sergk30@gmail.com>
2023-04-16 12:28:58 +0300

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('button[data-start]');
+const daysElement = document.querySelector('span[data-days]');
+const hoursElement = document.querySelector('span[data-hours]');
+const minutesElement = document.querySelector('span[data-minutes]');
+const secondsElement = document.querySelector('span[data-seconds]');
+
+let timerId = null;
 
 const options = {
   enableTime: true,
@@ -7,15 +16,53 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    startBtn.setAttribute('disabled', 'disabled');
+
+    if (selectedDates[0] > new Date()) {
+      startBtn.removeAttribute('disabled');
+
+      startBtn.addEventListener('click', () => {
+        timerId = setInterval(() => {
+          const differenceTime = selectedDates[0] - new Date();
+
+          outputTime(convertMs(differenceTime));
+
+          if (differenceTime < 1000) {
+            clearInterval(timerId);
+          }
+        }, 1000);
+      });
+
+      return;
+    }
+
+    Notiflix.Notify.failure('Please choose a date in the future');
   },
 };
-// -------
-// 1-Знати детлайн
-// 2-поточна дата
-// 3-різниця 1 і 2
-// 4- конвертувати різницю
-// 5- відобразити
 
-const deadline = new Date();
-const todey = new Date();
+flatpickr('input#datetime-picker', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function outputTime({ days, hours, minutes, seconds }) {
+  daysElement.textContent = addLeadingZero(days);
+  hoursElement.textContent = addLeadingZero(hours);
+  minutesElement.textContent = addLeadingZero(minutes);
+  secondsElement.textContent = addLeadingZero(seconds);
+}
